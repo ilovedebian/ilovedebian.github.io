8b255a08298a0148f9a79a0babc68020adf29814
ilovedebian <kozlovxyz@gmail.com>
2025-09-05 07:24:50 +0700

    Russian
    new page for changing language and Russian version of the site

---
+++ ru/http404.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>404 | Reunion7</title>
+        <meta property="og:title" content="Reunion7">
+        <meta property="og:description" content="Добро пожаловать в Reunion7 — модификацию Windows, которая стремится быть неотличимой от Windows 7.">
+        <meta property="og:image" content="https://reunion7.com/Assets/seo.png">
+        <meta property="og:url" content="https://reunion7.com/ru/404.html">
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="Reunion7">    
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="Assets/icon.png">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="body-content">
+        <div class="navbar">
+                <div class="top-header">
+                    <p class="language-header">Россия - Русский</p>
+                    <a href="region.html">Изменить</a>
+                    |
+                    <a href="projects.html">Другие проекты</a>
+                </div>
+                <div class="bottom-header">
+                    <a href="index.html">
+                        <img src="Assets/long-logo.svg" alt="Логотип Reunion" class="reunion-logo">
+                    </a> 
+                    <div class="navlinks">
+                        <a href="index.html">Главная</a>
+                        <a href="downloads.html">Загрузки</a>
+                        <a href="credits.html">Благодарности</a>
+                        <a href="changelog.html">История изменений</a>
+                        <a href="https://github.com/reunion-7-mod">GitHub</a>
+                        <a href="https://discord.gg/dXVHnz2My9">Discord</a>
+                    </div>
+                </div>
+            </div>
+            <div class="content-padded footer-404">
+                <h1 class="header-404">Страница не найдена</h1>
+                <p>Запрашиваемая страница не существует или временно недоступна. <a href="index.html">Вернуться на главную</a></p>
+            </div>
+            <footer>
+                <div class="copyright">
+                    <img src="Assets/long-logo.svg" alt="Логотип Reunion" class="reunion-logo-footer">
+                    <p>© 2025 Reunion7</p>
+                </div>
+                <div class="right-side">
+                    <p class="psa">Этот проект не связан с Microsoft. Windows и её логотип являются собственностью Microsoft.</p>
+                    <img src="Assets/teamlogo.svg" alt="Логотип команды Reunion" class="team-logo">
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
